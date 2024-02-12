4326e2a063d2c1f7cfd6151b8972fddb3775babb
galtzhori <tzhori.gal@gmail.com>
2024-02-12 19:01:40 +0200

    added trajectory for shooting

--- Assets/Scripts/Player/BeansShooting.cs
+++ Assets/Scripts/Player/BeansShooting.cs
@@ -12,100 +12,72 @@ public class BeansShooting : MonoBehaviour
     [SerializeField] private GameObject aimPoint;
     [SerializeField] private GameObject shootingPoint;
     [SerializeField] private GameObject gun;
+    [SerializeField] private GameObject trajectoryPointPrefab;
+    [SerializeField] private int trajectoryPointsCount = 20;
+    private GameObject[] trajectoryPoints;
+
 
     [SerializeField] private GameObject beanPrefab;
 
     [SerializeField] private LayerMask platformLayerMask;
-    
+
     private Camera _mainCamera;
+    private Rigidbody2D _rigidBody;
 
     private SpriteRenderer _playerSpriteRenderer;
 
 
     void Awake()
     {
+        _rigidBody = GetComponent<Rigidbody2D>();
         _mainCamera = Camera.main;
-        _playerSpriteRenderer= GetComponent<SpriteRenderer>();
+        _playerSpriteRenderer = GetComponent<SpriteRenderer>();
+        trajectoryPoints = new GameObject[trajectoryPointsCount];
+        for (int i = 0; i < trajectoryPoints.Length; i++)
+        {
+            trajectoryPoints[i] = Instantiate(trajectoryPointPrefab, transform.position, Quaternion.identity);
+            trajectoryPoints[i].SetActive(false);
+        }
     }
 
-    
 
-   
     private void Update()
     {
         ShootBeans();
     }
 
-    void HandleGunRotation()
+    Vector2 TrajectoryPointsPosition(float t)
     {
-        
+        Vector3 direction = (transform.localScale.x >= 0) ? new Vector3(1, 1, 0) : new Vector3(-1, 1, 0);
+
+        Vector2 currentPointPosition = (Vector2)shootingPoint.transform.position +
+                                       ((Vector2)direction * (shootingForce * t)+_rigidBody.velocity) +
+                                       (Physics2D.gravity * (t * t * 0.5f));
+        return currentPointPosition;
     }
-    
+
+    private void ShowTrajectoryPoints()
+    {
+        float timeStep = 0.1f;
+        for (int i = 0; i < trajectoryPoints.Length; i++)
+        {
+            trajectoryPoints[i].transform.position = TrajectoryPointsPosition(i * timeStep);
+            trajectoryPoints[i].SetActive(true);
+        }
+    }
+
     void ShootBeans()
     {
         if (Input.GetButtonDown("Fire1"))
         {
             // transform.Rotate(Vector3.up * speed * Time.deltaTime);
-            GameObject projectile = Instantiate(beanPrefab, shootingPoint.transform.position, Quaternion.identity);
+            GameObject projectile = Instantiate(beanPrefab, shootingPoint.transform.position, gun.transform.rotation);
             Rigidbody2D rb = projectile.GetComponent<Rigidbody2D>();
-            // use the gun rotation to determine the direction of the projectile
-            
-            
-            Debug.Log(gun.transform.eulerAngles);
-            Vector3 direction = (transform.localScale.x>=0)? new Vector3(1,1,0): new Vector3(-1,1,0);
-            rb.velocity = transform.forward * shootingForce;        
+
+
+            Vector3 direction = (transform.localScale.x >= 0) ? new Vector3(1, 1, 0) : new Vector3(-1, 1, 0);
+            rb.velocity = direction * shootingForce + (Vector3)_rigidBody.velocity;
+            ShowTrajectoryPoints();
         }
     }
-    // void LaunchProjectile()
-    // {
-    //     Vector3 newAimPoint = aimPoint.transform.position;
-    //     float distance = _playerSpriteRenderer.flipX? -aimDistance: aimDistance;
-    //     newAimPoint+= new Vector3(distance,0,0);
-    //     aimPoint.transform.position =newAimPoint;
-    //     Ray ray = _mainCamera.ScreenPointToRay(aimPoint.transform.position);
-    //     RaycastHit hit;
-    //
-    //     if (Physics.Raycast(ray, out hit, 100f,platformLayerMask))
-    //     {
-    //         aimPoint.SetActive(true);
-    //         float timeToTarget = 1f;
-    //         Vector3 velocity = CalculateVelocity(hit.point, shootingPoint.transform.position, timeToTarget);
-    //         Debug.Log(velocity);
-    //         // check if player pressed ctrl
-    //         
-    //         if (Input.GetButtonDown("Fire1"))
-    //         {
-    //             GameObject projectile = Instantiate(beanPrefab, shootingPoint.transform.position, Quaternion.identity);
-    //             Rigidbody2D rb = projectile.GetComponent<Rigidbody2D>();
-    //             rb.velocity = velocity;
-    //         }
-    //     }
-    //     
-    // }
-    
-    
-    
-    
-    
-    
-    
-    
-    // Vector3 CalculateVelocity(Vector3 target, Vector3 origin, float time)
-    // {
-    //     Vector3 distance = target - origin;
-    //     Vector3 distanceXZ = distance;
-    //     distanceXZ.y = 0f;
-    //     
-    //     float Sy = distance.y;
-    //     float Sxz = distanceXZ.magnitude;
-    //     
-    //     float Vxz = Sxz / time;
-    //     float Vy = Sy / time + .5f * Mathf.Abs(Physics.gravity.y) * time;
-    //     
-    //     Vector3 result = distanceXZ.normalized;
-    //     result *= Vxz;
-    //     result.y = Vy;
-    //     
-    //     return result;
-    // }
 }
