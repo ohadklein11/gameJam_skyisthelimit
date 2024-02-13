850e5155337d5f9012653de3a785f23c50592b26
galtzhori <tzhori.gal@gmail.com>
2024-02-13 13:01:19 +0200

    working on vines

--- Assets/Scripts/Bullets/Bean.cs
+++ Assets/Scripts/Bullets/Bean.cs
@@ -5,8 +5,59 @@ using UnityEngine;
 
 public class Bean : MonoBehaviour
 {
+    private SpriteRenderer _spriteRenderer;
+    public bool Grounded { get; private set; }
+    [SerializeField] private LayerMask _groundLayerMask;
+    [SerializeField] private LayerMask _VineLayerMask;
+
+    [SerializeField] private float distanceToUpperPlatform = 20f;
+    [SerializeField] private float platformWidth = 5f;
+    
+    void Awake()
+    {
+        _spriteRenderer = GetComponent<SpriteRenderer>();
+        Grounded = false;
+    }
     private void OnBecameInvisible()
     {
         Destroy(gameObject);
+    }
+
+    void Update()
+    {
+
+        CheckGrounded();
+    }
+    
+    private void CheckGrounded()
+    {
+        Vector3 position = transform.position;
+        Vector3 localScale = transform.localScale;
+        float distance = .4f * localScale.y;
+        Bounds spriteBounds = _spriteRenderer.bounds;
+        Vector3 BeanBottomCenter = position - new Vector3(0, (spriteBounds.max.y - spriteBounds.min.y) / 2, 0);
+        var hit = Physics2D.Raycast(BeanBottomCenter, Vector2.down, distance, _groundLayerMask);
+        Debug.DrawRay(BeanBottomCenter, Vector3.down * distance, Color.red);
+        if (hit&& !Grounded)
+        {
+            GrowVine();
+            Grounded = true;
+        }
+    }
+    
+    private void GrowVine()
+    {
+        Vector3 position = transform.position;
+
+        var hit = Physics2D.Raycast(position, Vector2.up, distanceToUpperPlatform, _groundLayerMask);
+        var hitLeft = Physics2D.Raycast(position, Vector2.left, platformWidth/2, _VineLayerMask);
+        var hitRight = Physics2D.Raycast(position, Vector2.right, platformWidth/2, _VineLayerMask);
+
+        if (hit&& !hitLeft && !hitRight)
+        {
+            GetComponent<Rigidbody2D>().velocity = Vector2.zero;
+            Debug.Log("Vine Grown");
+
+        }
     }
 }
