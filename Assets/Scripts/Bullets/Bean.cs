fc99364c5aee58be731ac54e123018b3d49662c2
galtzhori <tzhori.gal@gmail.com>
2024-02-15 18:53:46 +0200

    delete egg when outside of camera

--- Assets/Scripts/Bullets/Bean.cs
+++ Assets/Scripts/Bullets/Bean.cs
@@ -27,25 +27,6 @@ public class Bean : MonoBehaviour
     void Update()
     {
         CheckGrounded();
-
-        // if camera is 1.5 screens on the left of the bean, destroy the bean
-        if (transform.position.x < _mainCamera.transform.position.x - _mainCamera.orthographicSize * _mainCamera.aspect * 1.5f)
-        {
-            Destroy(gameObject);
-        }
-        // if camera is 1.5 screens on the right of the bean, destroy the bean
-        if (transform.position.x > _mainCamera.transform.position.x + _mainCamera.orthographicSize * _mainCamera.aspect * 1.5f)
-        {
-            Destroy(gameObject);
-        }
-        // if camera is 1.5 screens above the bean, destroy the bean
-        if (transform.position.y > _mainCamera.transform.position.y + _mainCamera.orthographicSize * 1.5f)
-        {
-            Destroy(gameObject);
-        }
-
-        
-        
     }
     
     private void CheckGrounded()
