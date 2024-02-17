89be7418eb67b3a3a4559391587eb061db3f832e
ohadklein <ohad.klein@mail.huji.ac.il>
2024-02-17 21:42:55 +0200

    Boss floor collider updated

--- Assets/Scripts/Cutscenes/GiantFight.cs
+++ Assets/Scripts/Cutscenes/GiantFight.cs
@@ -1,10 +1,17 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class GiantFight : MonoBehaviour
 {
     [SerializeField] private GameObject _giant;
     [SerializeField] private GameObject _player;
+    [SerializeField] private GameObject _giantFightTrigger;
     
+    
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.G)) // temp to teleport to giant
+        {
+            _player.transform.position = _giantFightTrigger.transform.position + new Vector3(2, 1, 0);
+        }
+    }
 }
