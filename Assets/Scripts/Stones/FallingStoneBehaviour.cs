51b27c288161dcbf6e6b2d3f5373a1972b561f37
galtzhori <tzhori.gal@gmail.com>
2024-02-18 19:37:49 +0200

    added camera shake when stone hits ground

--- Assets/Scripts/Stones/FallingStoneBehaviour.cs
+++ Assets/Scripts/Stones/FallingStoneBehaviour.cs
@@ -1,22 +1,31 @@
 using System.Collections;
 using System.Collections.Generic;
+using Cinemachine;
 using Giant;
 using UnityEngine;
 
 public class FallingStoneBehaviour : GiantThrowableBehavior
 {
-    private CameraShake _cameraShake;
-
+    private CinemachineVirtualCamera _virtualCamera;
+    [SerializeField] private float shakeDuration = 1f;
+    [SerializeField] private float shakeMagnitude = 2f;
     void Awake()
     {
-        _cameraShake = Camera.main.GetComponent<CameraShake>();
+        _virtualCamera = FindObjectOfType<CinemachineVirtualCamera>();
     }
+    
     protected override void OnCollisionEnter2D(Collision2D other)
     {
+        Debug.Log(other.gameObject.layer);
         if (other.gameObject.layer == LayerMask.NameToLayer("Ground"))
         {
-            StartCoroutine(_cameraShake.Shake(0.1f, 0.1f));}
+            Debug.Log("FallingStone");
+            _virtualCamera.GetComponent<CameraShake>().Shake(shakeMagnitude, shakeDuration);
+        }
         else
+        {
+            Debug.Log("GiantThrowable");
             base.OnCollisionEnter2D(other);
+        }
     }
 }
--- Assets/Scripts/Utils/CameraShake.cs
+++ Assets/Scripts/Utils/CameraShake.cs
@@ -1,23 +1,40 @@
 using System.Collections;
 using System.Collections.Generic;
+using Cinemachine;
 using UnityEngine;
 
 public class CameraShake : MonoBehaviour
 {
-    public IEnumerator Shake(float duration, float magnitude)
+    private CinemachineVirtualCamera _virtualCamera;
+    private float _shakeTimer;
+    private float _shakeTimerTotal;
+    private float _shakeIntensity;
+    
+    private void Awake()
     {
-        Vector3 orignalPosition = transform.position;
-        float elapsed = 0f;
-        
-        while (elapsed < duration)
+        _virtualCamera = GetComponent<CinemachineVirtualCamera>();
+    }
+    
+    public void Shake(float intensity, float time)
+    {
+        CinemachineBasicMultiChannelPerlin cinemachineBasicMultiChannelPerlin = _virtualCamera.GetCinemachineComponent<CinemachineBasicMultiChannelPerlin>();
+        cinemachineBasicMultiChannelPerlin.m_AmplitudeGain = intensity;
+        _shakeTimer = time;
+        _shakeTimerTotal = time;
+        _shakeIntensity= intensity;
+    }
+    
+    private void Update()
+    {
+        if (_shakeTimer > 0)
         {
-            float x = Random.Range(-1f, 1f) * magnitude;
-            float y = Random.Range(-1f, 1f) * magnitude;
-
-            transform.position = new Vector3(x, y, -10f);
-            elapsed += Time.deltaTime;
-            yield return 0;
+            _shakeTimer -= Time.deltaTime;
+            if (_shakeTimer <= 0)
+            {
+                CinemachineBasicMultiChannelPerlin cinemachineBasicMultiChannelPerlin = _virtualCamera.GetCinemachineComponent<CinemachineBasicMultiChannelPerlin>();
+                cinemachineBasicMultiChannelPerlin.m_AmplitudeGain = 0;
+                Mathf.Lerp(_shakeIntensity,0,1-(_shakeTimer/_shakeTimerTotal));
+            }
         }
-        transform.position = orignalPosition;
     }
 }
