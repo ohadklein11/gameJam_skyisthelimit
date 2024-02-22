839bda78101022a701ded3daecd7cd9148c4cf7d
galtzhori <tzhori.gal@gmail.com>
2024-02-22 16:04:41 +0200

    fixed player moving between vines while pressing horizontal key

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -7,34 +7,24 @@ namespace Player
 {
     public class PlayerMovement : MonoBehaviour
     {
+        [SerializeField] private float minFallDamage;
+        [SerializeField] private float movementSpeed;
+
         [SerializeField]
-        private float minFallDamage;
-        [SerializeField]
-        private float movementSpeed;
-        [SerializeField] 
-        private int numFramesUntilMaxSlowdown;  // e.g. after 20 frames of moving, the player will slide to a stop upon releasing the movement key
-        [SerializeField]
-        private float groundCheckRadius;
-        [SerializeField]
-        private float jumpForce;
-        [SerializeField]
-        private float slopeCheckDistance;
-        [SerializeField]
-        private float maxSlopeAngle;
-        [SerializeField]
-        private Transform groundCheck;
-        [SerializeField]
-        private LayerMask whatIsGround;
-        [SerializeField] 
-        private LayerMask whatIsVine;
-        [SerializeField]
-        private PhysicsMaterial2D noFriction;
-        [SerializeField]
-        private PhysicsMaterial2D fullFriction;
-        [SerializeField]
-        private float climbSpeed;
-        [SerializeField] 
-        private int minNumFramesForClimbing;
+        private int
+            numFramesUntilMaxSlowdown; // e.g. after 20 frames of moving, the player will slide to a stop upon releasing the movement key
+
+        [SerializeField] private float groundCheckRadius;
+        [SerializeField] private float jumpForce;
+        [SerializeField] private float slopeCheckDistance;
+        [SerializeField] private float maxSlopeAngle;
+        [SerializeField] private Transform groundCheck;
+        [SerializeField] private LayerMask whatIsGround;
+        [SerializeField] private LayerMask whatIsVine;
+        [SerializeField] private PhysicsMaterial2D noFriction;
+        [SerializeField] private PhysicsMaterial2D fullFriction;
+        [SerializeField] private float climbSpeed;
+        [SerializeField] private int minNumFramesForClimbing;
 
         private float _xInput;
         private float _yInput;
@@ -58,7 +48,7 @@ namespace Player
         private bool _wasGrounded;
         private int _numFramesSinceEnteringClimbing;
         private IClimbable _climbable;
-        
+
         private bool IsFalling => (!_isGrounded && !_isClimbing && _rb.velocity.y < 0.0f);
 
         private bool IsTryingToClimb => (_isGrounded && _yInput > 0.0f) || (!_isGrounded && _yInput != 0.0f);
@@ -86,7 +76,7 @@ namespace Player
 
         private void Update()
         {
-            CheckInput();     
+            CheckInput();
         }
 
         private void FixedUpdate()
@@ -96,7 +86,6 @@ namespace Player
             CheckClimb();
             ApplyMovement();
             CheckFalling();
-
         }
 
         private void CheckFalling()
@@ -105,10 +94,11 @@ namespace Player
             {
                 _fallingMaxHeight = transform.position.y;
             }
-            else if(IsFalling && transform.position.y > _fallingMaxHeight)
+            else if (IsFalling && transform.position.y > _fallingMaxHeight)
             {
                 _fallingMaxHeight = transform.position.y;
             }
+
             if (!_wasGrounded && _isGrounded)
             {
                 if (_wasFalling)
@@ -116,6 +106,7 @@ namespace Player
                     TakeFallDamage();
                 }
             }
+
             _wasFalling = IsFalling;
             _wasGrounded = _isGrounded;
         }
@@ -123,7 +114,7 @@ namespace Player
         private void TakeFallDamage()
         {
             int fallDamage = (int)(_fallingMaxHeight - transform.position.y);
-            if (fallDamage>minFallDamage)
+            if (fallDamage > minFallDamage)
             {
                 Debug.Log("Player took alot of fall damage");
                 EventManagerScript.Instance.TriggerEvent(EventManagerScript.PlayerGotHit, fallDamage);
@@ -145,22 +136,23 @@ namespace Player
             }
 
             HandleClimbing();
-            
+
             if (Input.GetButtonDown("Jump"))
             {
                 Jump();
             }
-
         }
 
         private void HandleClimbing()
         {
             if (_canClimb && (IsTryingToClimb || (_isClimbing && !_isGrounded && !_isJumping)))
-            {  // climb
+            {
+                // climb
                 if (!_isClimbing)
                 {
                     _numFramesSinceEnteringClimbing = minNumFramesForClimbing;
                 }
+
                 _isClimbing = true;
                 var position = transform.position;
                 try
@@ -171,17 +163,19 @@ namespace Player
                     // prevent climbing up when reaching end of vine
                     if (_yInput > 0 && !Input.GetButton("Jump"))
                     {
-                        float playerHalfHeight = _spriteRenderer.bounds.size.y/2;
+                        float playerHalfHeight = _spriteRenderer.bounds.size.y / 2;
                         RaycastHit2D vineHitUp = Physics2D.Raycast(
-                            new Vector2(transform.position.x, transform.position.y+playerHalfHeight+0.1f), 
+                            new Vector2(transform.position.x, transform.position.y + playerHalfHeight + 0.1f),
                             Vector2.up, .1f, whatIsVine);
                         if (!vineHitUp)
                         {
                             _yInput = 0;
                         }
                     }
+
                     _rb.velocity = new Vector2(0, _yInput * climbSpeed);
-                } catch (MissingReferenceException e)
+                }
+                catch (MissingReferenceException e)
                 {
                     _isClimbing = false;
                 }
@@ -190,6 +184,7 @@ namespace Player
             {
                 _isClimbing = false;
             }
+
             if (_isClimbing && transform.parent != _climbable.GetParentTransform())
             {
                 transform.SetParent(_climbable.GetParentTransform());
@@ -198,24 +193,25 @@ namespace Player
             {
                 transform.SetParent(null);
             }
+
             _isEnteringClimbing = _numFramesSinceEnteringClimbing > 0;
             _numFramesSinceEnteringClimbing = Mathf.Max(0, _numFramesSinceEnteringClimbing - 1);
         }
 
         private void CheckGround()
         {
-            _isGrounded = !_isEnteringClimbing && Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, whatIsGround);
+            _isGrounded = !_isEnteringClimbing &&
+                          Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, whatIsGround);
 
-            if(_rb.velocity.y <= 0.0f)
+            if (_rb.velocity.y <= 0.0f)
             {
                 _isJumping = false;
             }
 
-            if(!_isJumping && (_isClimbing || (_isGrounded && _slopeDownAngle <= maxSlopeAngle)))
+            if (!_isJumping && (_isClimbing || (_isGrounded && _slopeDownAngle <= maxSlopeAngle)))
             {
                 _canJump = true;
             }
-
         }
 
         private void SlopeCheck()
@@ -232,13 +228,12 @@ namespace Player
         {
             RaycastHit2D slopeHitFront = Physics2D.Raycast(checkPos, transform.right, slopeCheckDistance, whatIsGround);
             RaycastHit2D slopeHitBack = Physics2D.Raycast(checkPos, -transform.right, slopeCheckDistance, whatIsGround);
-        
+
             if (slopeHitFront)
             {
                 _isOnSlope = true;
 
                 _slopeSideAngle = Vector2.Angle(slopeHitFront.normal, Vector2.up);
-
             }
             else if (slopeHitBack)
             {
@@ -251,31 +246,28 @@ namespace Player
                 _slopeSideAngle = 0.0f;
                 _isOnSlope = false;
             }
-
         }
 
         private void SlopeCheckVertical(Vector2 checkPos)
-        {      
+        {
             RaycastHit2D hit = Physics2D.Raycast(checkPos, Vector2.down, slopeCheckDistance, whatIsGround);
             Debug.DrawRay(checkPos, Vector2.down, Color.red);
 
             if (hit)
             {
-
-                _slopeNormalPerp = Vector2.Perpendicular(hit.normal).normalized;            
+                _slopeNormalPerp = Vector2.Perpendicular(hit.normal).normalized;
 
                 _slopeDownAngle = Vector2.Angle(hit.normal, Vector2.up);
 
-                if(_slopeDownAngle != _lastSlopeAngle)
+                if (_slopeDownAngle != _lastSlopeAngle)
                 {
                     _isOnSlope = true;
-                }                       
+                }
 
                 _lastSlopeAngle = _slopeDownAngle;
-           
+
                 Debug.DrawRay(hit.point, _slopeNormalPerp, Color.blue);
                 Debug.DrawRay(hit.point, hit.normal, Color.green);
-
             }
 
             if (_slopeDownAngle > maxSlopeAngle || _slopeSideAngle > maxSlopeAngle)
@@ -299,12 +291,20 @@ namespace Player
 
         private void CheckClimb()
         {
-            float playerHalfHeight = _spriteRenderer.bounds.size.y/2;
-            RaycastHit2D vineHit = Physics2D.Raycast(
-                new Vector2(transform.position.x - _playerXradius * _facingDirection, transform.position.y), 
-                Vector2.right * _facingDirection, 2*_playerXradius, whatIsVine);
-            if (vineHit.collider != null && vineHit.collider.TryGetComponent(out IClimbable climbable))
+            // RaycastHit2D vineHit = Physics2D.Raycast(
+            //     new Vector2(transform.position.x - _playerXradius * _facingDirection, transform.position.y), 
+            //     Vector2.right * _facingDirection, 2*_playerXradius, whatIsVine);
+            RaycastHit2D vineHitLeft = Physics2D.Raycast(
+                new Vector2(transform.position.x, transform.position.y),
+                Vector2.right * _facingDirection, _playerXradius, whatIsVine);
+            RaycastHit2D vineHitRight = Physics2D.Raycast(
+                new Vector2(transform.position.x, transform.position.y),
+                Vector2.left * _facingDirection, _playerXradius, whatIsVine);
+            IClimbable climbable1 = null, climbable2 = null;
+            if ((vineHitLeft.collider != null && vineHitLeft.collider.TryGetComponent(out climbable1)) ||
+                (vineHitRight.collider != null && vineHitRight.collider.TryGetComponent(out climbable2)))
             {
+                var climbable = climbable1 ?? climbable2;
                 _canClimb = true;
                 _climbable = climbable;
             }
@@ -321,7 +321,7 @@ namespace Player
             {
                 _canJump = false;
                 _isJumping = true;
-                var newJumpForce = _isGrounded? jumpForce - _newVelocity.y*.8f : jumpForce;
+                var newJumpForce = _isGrounded ? jumpForce - _newVelocity.y * .8f : jumpForce;
                 _newForce.Set(0.0f, newJumpForce);
                 _rb.AddForce(_newForce, ForceMode2D.Impulse);
             }
@@ -344,6 +344,7 @@ namespace Player
                     _timeMultiplier = 0.0f;
                 }
             }
+
             _velocityMultiplier *= _timeMultiplier;
             if (_isGrounded && !_isOnSlope && !_isJumping) //if not on slope
             {
@@ -352,7 +353,8 @@ namespace Player
             }
             else if (_isGrounded && _isOnSlope && _canWalkOnSlope && !_isJumping) //If on slope
             {
-                _newVelocity.Set(movementSpeed * _slopeNormalPerp.x * -_velocityMultiplier, movementSpeed * _slopeNormalPerp.y * -_velocityMultiplier);
+                _newVelocity.Set(movementSpeed * _slopeNormalPerp.x * -_velocityMultiplier,
+                    movementSpeed * _slopeNormalPerp.y * -_velocityMultiplier);
                 _rb.velocity = _newVelocity;
             }
             else if (!_isGrounded && !_isClimbing) //If in air
@@ -360,11 +362,11 @@ namespace Player
                 _newVelocity.Set(movementSpeed * _velocityMultiplier, _rb.velocity.y);
                 _rb.velocity = _newVelocity;
             }
-        
-            // gravity handling
-            var gravity = _isClimbing? 0f : _isJumping? Input.GetButton("Jump") ? highJumpGravity : jumpGravity : fallGravity;
-            _rb.gravityScale = gravity;
 
+            // gravity handling
+            var gravity = _isClimbing ? 0f :
+                _isJumping ? Input.GetButton("Jump") ? highJumpGravity : jumpGravity : fallGravity;
+            _rb.gravityScale = gravity;
         }
 
         private void Flip()
@@ -374,7 +376,7 @@ namespace Player
             flipScale.x *= -1;
             transform.localScale = flipScale;
         }
-        
+
         public void StopClimbing()
         {
             _canClimb = false;
@@ -387,4 +389,3 @@ namespace Player
         }
     }
 }
-
