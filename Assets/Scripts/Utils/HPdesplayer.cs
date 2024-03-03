465363c6d932c67a2704cdd998f3087f122d6aab
ohadklein <ohad.klein@mail.huji.ac.il>
2024-03-03 21:29:32 +0200

    HP bar

--- Assets/Scripts/Utils/HPdesplayer.cs
+++ Assets/Scripts/Utils/HPdesplayer.cs
@@ -1,5 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
+using DG.Tweening;
+using DG.Tweening.Core;
+using DG.Tweening.Plugins.Options;
 using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
@@ -7,22 +10,36 @@ using Utils;
 
 public class HPdesplayer : MonoBehaviour
 {
-    [SerializeField] private Slider hpSlider;
-    [SerializeField] private TextMeshProUGUI hpText;
+    [SerializeField] private Image hpSlider;
     private GameData _gameData;
-    private int _maxPlayerLifePoints;
+
+    private int _minHP = 0;
+    private int _maxHP;
+    private int _curHP;
+    private float _minFill = 0.32f;
+    private float _maxFill = 0.946f;
+    private float _curFill;
+    private TweenerCore<float,float,FloatOptions> tween;
+
     // Start is called before the first frame update
     void Start()
     {
         _gameData = GameData.Instance;
         _gameData.RestartPlayerHealth();
-        _maxPlayerLifePoints = _gameData.maxPlayerLifePoints;
-        hpSlider.maxValue = _maxPlayerLifePoints;
-        hpText.text = _gameData.GetPlayerHealth().ToString() + "/" + _maxPlayerLifePoints.ToString();
+        _maxHP = _gameData.maxPlayerLifePoints;
+        _curHP = _maxHP;
+        _curFill = _maxFill;
+        hpSlider.fillAmount = _curFill;
     }
     void Update()
     {
-        hpSlider.value = _gameData.GetPlayerHealth();
-        hpText.text = _gameData.GetPlayerHealth().ToString() + "/" + _maxPlayerLifePoints.ToString();
+        var curHP = _gameData.GetPlayerHealth();
+        if (_curHP != curHP)
+        {
+            var newFill = Mathf.Lerp(_minFill, _maxFill, (float)curHP / _maxHP);
+            tween?.Kill();
+            tween = hpSlider.DOFillAmount(newFill, 1f).SetEase(Ease.OutCubic);
+            _curHP = curHP;
+        }
     }
 }
