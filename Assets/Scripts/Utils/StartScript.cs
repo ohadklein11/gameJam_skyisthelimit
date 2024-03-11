0bd16f1183c224f62aa30a57e9506a73090f327e
galtzhori <tzhori.gal@gmail.com>
2024-03-11 22:45:47 +0200

    logo fades in

--- Assets/Scripts/Utils/StartScript.cs
+++ Assets/Scripts/Utils/StartScript.cs
@@ -1,15 +1,21 @@
+using DG.Tweening;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 using Utils;
 
 public class StartScript : MonoBehaviour
 {
     public GameObject startPanel;
     [SerializeField] private AudioSource audioSource;
+    [SerializeField] private Image logo;
     // Start is called before the first frame update
     void Start()
     {
         GameData.isGameStopped = true;
         startPanel.SetActive(true);
+        logo.DOFade(1, 3f).SetEase(Ease.OutFlash);
+
     }
 
     // Update is called once per frame
