300588567b4119a32ff8b61aa1c8845f6020ab45
galtzhori <tzhori.gal@gmail.com>
2024-03-13 13:23:03 +0200

    added health bean and game stop

--- Assets/Scripts/Health/HealthFiller.cs
+++ Assets/Scripts/Health/HealthFiller.cs
@@ -10,9 +10,11 @@ public class HealthFiller : MonoBehaviour
 
     void Start()
     {
-        transform.DOScale(new Vector3(.35f, .35f, 1f), 1.25f).SetLoops(-1, LoopType.Yoyo).SetEase(Ease.InOutSine);
-        // transform.DOLocalMoveY(transform.localPosition.y + 0.2f, 1f).SetLoops(-1, LoopType.Yoyo).SetEase(Ease.InOutSine);
-
+        // transform.DOScale(new Vector3(.35f, .35f, 1f), 1.25f).SetLoops(-1, LoopType.Yoyo).SetEase(Ease.InOutSine);
+        transform.DOLocalMoveY(transform.localPosition.y + 0.2f, 2.3f).SetLoops(-1, LoopType.Yoyo).SetEase(Ease.InOutSine);
+        // set end color to 328E35
+        Color endColor = new Color(0.1960785f, 0.5568628f, 0.2078432f, 1f);
+        GetComponent<SpriteRenderer>().DOBlendableColor(endColor, 1f).SetEase(Ease.OutCubic).SetLoops(-1, LoopType.Yoyo);
     }
 
     private void OnTriggerEnter2D(Collider2D other)
